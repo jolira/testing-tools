f02f7811d4d9f1ff7b0beee76019490ce2de1890
joachim.kainz <joachim.kainz@c86e12c4-a0bc-11de-afa9-e300858c10cc>
2010-06-13 04:34:10 +0000

    re-enable tests.
    git-svn-id: http://jolira-tools.googlecode.com/svn/testing-tools/trunk@2566 c86e12c4-a0bc-11de-afa9-e300858c10cc

--- src/test/java/com/jolira/testing/WebServerEmulatorTest.java
+++ src/test/java/com/jolira/testing/WebServerEmulatorTest.java
@@ -142,70 +142,68 @@ public class WebServerEmulatorTest {
         }
     }
 
-    // DISABLED BECAUSE OF MAVEN ISSUE
-    // /**
-    // * Test method for {@link WebServerEmulator#createServer(int)}.
-    // *
-    // * @throws Exception
-    // */
-    // @Test
-    // public void testResponse2() throws Exception {
-    // final WebServerEmulator svr = new WebServerEmulator() {
-    // @Override
-    // protected void handle(final String target,
-    // final HttpServletRequest request,
-    // final HttpServletResponse response) throws IOException,
-    // ServletException {
-    // assertEquals(TEST_TARGET, target);
-    //
-    // respond(response, "response.xml");
-    // }
-    // };
-    //
-    // svr.start();
-    //
-    // final String name = svr.getName();
-    // final URL url = new URL("http://" + name + TEST_TARGET);
-    // final URLConnection conn = url.openConnection();
-    // final InputStream in = conn.getInputStream();
-    // final BufferedReader rd = new BufferedReader(new InputStreamReader(in));
-    //
-    // try {
-    // final String line1 = rd.readLine();
-    // final String line2 = rd.readLine();
-    //
-    // assertEquals("<response>Hello World</response>", line1);
-    // assertNull(line2);
-    // } finally {
-    // rd.close();
-    // svr.stop();
-    // }
-    // }
+    /**
+     * Test method for {@link WebServerEmulator#createServer(int)}.
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void testResponse2() throws Exception {
+        final WebServerEmulator svr = new WebServerEmulator() {
+            @Override
+            protected void handle(final String target,
+                    final HttpServletRequest request,
+                    final HttpServletResponse response) throws IOException,
+                    ServletException {
+                assertEquals(TEST_TARGET, target);
 
-    // DISABLED BECAUSE OF MAVEN ISSUE
-    // /**
-    // * Test method for {@link WebServerEmulator#createServer(int)}.
-    // *
-    // * @throws Exception
-    // */
-    // @Test(expected = IllegalStateException.class)
-    // public void testStartServerTwice() throws Exception {
-    // final WebServerEmulator svr = new WebServerEmulator() {
-    // @Override
-    // protected Server createServer(final int port) {
-    // return new Server();
-    // }
-    //
-    // @Override
-    // protected void handle(final String target,
-    // final HttpServletRequest request,
-    // final HttpServletResponse response) throws IOException,
-    // ServletException {
-    // fail();
-    // }
-    // };
-    //
-    // svr.start();
-    // svr.start();
-    // }
+                respond(response, "response.xml");
+            }
+        };
+
+        svr.start();
+
+        final String name = svr.getName();
+        final URL url = new URL("http://" + name + TEST_TARGET);
+        final URLConnection conn = url.openConnection();
+        final InputStream in = conn.getInputStream();
+        final BufferedReader rd = new BufferedReader(new InputStreamReader(in));
+
+        try {
+            final String line1 = rd.readLine();
+            final String line2 = rd.readLine();
+
+            assertEquals("<response>Hello World</response>", line1);
+            assertNull(line2);
+        } finally {
+            rd.close();
+            svr.stop();
+        }
+    }
+
+    /**
+     * Test method for {@link WebServerEmulator#createServer(int)}.
+     * 
+     * @throws Exception
+     */
+    @Test(expected = IllegalStateException.class)
+    public void testStartServerTwice() throws Exception {
+        final WebServerEmulator svr = new WebServerEmulator() {
+            @Override
+            protected Server createServer(final int port) {
+                return new Server();
+            }
+
+            @Override
+            protected void handle(final String target,
+                    final HttpServletRequest request,
+                    final HttpServletResponse response) throws IOException,
+                    ServletException {
+                fail();
+            }
+        };
+
+        svr.start();
+        svr.start();
+    }
 }
